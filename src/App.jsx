723f52e9bfd59276c4aaeee3d686879dc35cba1e
sk5s <samko5sam@tutanota.com>
2024-10-16 21:29:44 +0800

    🐛 Fix bug in gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
+      <HashRouter basename="/">
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
