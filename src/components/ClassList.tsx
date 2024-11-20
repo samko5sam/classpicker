8866ef1c251edd7901767eb87314b5339bad315a
leokao960811 <leokao960811@gmail.com>
2024-11-20 12:55:25 +0800

    Filter List update

--- src/components/ClassList.tsx
+++ src/components/ClassList.tsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react';
 import { ClassTable } from './ClassTable';
-import { useGlobalContext } from '@/context/GlobalContext';
+import { useGlobalContext } from '../context/GlobalContext';
 import { Loader2 } from 'lucide-react';
 import { FilterDropdown, SearchBar } from './SearchAndFilter';
-//import {listorder} from '@/constants/listorder'
+import { listorder } from '../constants/listorder';
 
 export interface Course {
   開課序號: number;
@@ -26,11 +26,10 @@ export const ClassList: React.FC = () => {
   const [itemsPerPage] = useState(10);
 
   // Get unique departments from the course data
-  const departmentOptions = [...new Set(courses.map(course => course.系所).filter(Boolean))];
-
+  const departmentOptions = listorder.map(item => item.key);
 
   //Filter department options based on the department search term
-  const filteredDepartmentOptions = departmentOptions.filter(department => 
+  const filteredDepartmentOptions = departmentOptions.filter(department =>
     department.toLowerCase().includes(departmentSearchTerm.toLowerCase())
   );
 
@@ -49,7 +48,7 @@ export const ClassList: React.FC = () => {
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const filteredCourses = courses.filter(course => {
     const lowerCaseSearchTerm = debouncedSearchTerm.toLowerCase();
-    const matchesSearch = 
+    const matchesSearch =
       (course.中文課程名稱 && course.中文課程名稱.toLowerCase().includes(lowerCaseSearchTerm)) ||
       (course.英文課程名稱 && course.英文課程名稱.toLowerCase().includes(lowerCaseSearchTerm)) ||
       (course.開課序號 && course.開課序號.toString().toLowerCase().includes(lowerCaseSearchTerm)) ||
@@ -58,7 +57,7 @@ export const ClassList: React.FC = () => {
     return matchesSearch && matchesDepartment;
   });
 
-  const currentCourses = filteredCourses.slice(indexOfFirstItem, indexOfLastItem)
+  const currentCourses = filteredCourses.slice(indexOfFirstItem, indexOfLastItem);
 
   // Calculate the total number of pages
   const totalPages = Math.ceil(filteredCourses.length / itemsPerPage);
@@ -68,7 +67,7 @@ export const ClassList: React.FC = () => {
 
   useEffect(() => {
     handlePageChange(1);
-  }, [totalPages])
+  }, [totalPages]);
 
   return (
     <div>
