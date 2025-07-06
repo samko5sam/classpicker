cebad3aef4488d993b21d3dba767f77cf7af44b6
sk5s <samko5sam@tutanota.com>
2025-07-06 16:20:02 +0800

    ✨ Update announcements and metadata for the 114 academic year, first semester

--- src/constants/Metadata.ts
+++ src/constants/Metadata.ts
@@ -1,5 +1,5 @@
-export const SemesterYear = '113';
-export const SemesterPart = '2'
+export const SemesterYear = '114';
+export const SemesterPart = '1'
 export const SemesterUpdate = '1'
 export const Semester = SemesterYear + SemesterPart;
-export const SemesterDescription = "NTNU 113學年度第2學期";
+export const SemesterDescription = "NTNU 114學年度第1學期";
