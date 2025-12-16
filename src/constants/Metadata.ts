c6ba89592b7e6958400108b881459dc3ac89f80c
sk5s <samko5sam@tutanota.com>
2025-12-16 09:13:54 +0800

    feat: update semester to 114-2
    Update Semester constants to 114-2 and add announcement id 7 for semester update.

--- src/constants/Metadata.ts
+++ src/constants/Metadata.ts
@@ -1,5 +1,5 @@
 export const SemesterYear = '114';
-export const SemesterPart = '1'
+export const SemesterPart = '2'
 export const SemesterUpdate = '1'
 export const Semester = SemesterYear + SemesterPart;
-export const SemesterDescription = "NTNU 114學年度第1學期";
+export const SemesterDescription = "NTNU 114學年度第2學期";
