a37d0a5b3c89678b2d037ccc99e4c7e1343cfb50
sk5s <samko5sam@tutanota.com>
2026-01-06 09:34:32 +0800

    Update course data for 114-2 semester

--- src/constants/Metadata.ts
+++ src/constants/Metadata.ts
@@ -1,5 +1,5 @@
 export const SemesterYear = '114';
 export const SemesterPart = '2'
-export const SemesterUpdate = '1'
+export const SemesterUpdate = '2'
 export const Semester = SemesterYear + SemesterPart;
 export const SemesterDescription = "NTNU 114學年度第2學期";
