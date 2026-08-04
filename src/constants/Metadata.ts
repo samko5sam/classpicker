fc920b1d0e35801a5767610ab42e492f952361e0
sk5s <samko5sam@tutanota.com>
2026-08-04 15:32:56 +0800

    Update announcements and metadata for the 115 academic year, first semester
    - Added a new announcement for the 115學年度第1學期.
    - Updated semester year, part, and description to reflect the new academic term.

--- src/constants/Metadata.ts
+++ src/constants/Metadata.ts
@@ -1,5 +1,5 @@
-export const SemesterYear = '114';
-export const SemesterPart = '2'
-export const SemesterUpdate = '2'
+export const SemesterYear = '115';
+export const SemesterPart = '1'
+export const SemesterUpdate = '1'
 export const Semester = SemesterYear + SemesterPart;
-export const SemesterDescription = "NTNU 114學年度第2學期";
+export const SemesterDescription = "NTNU 115學年度第1學期";
